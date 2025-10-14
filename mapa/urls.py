b3e41ab824ad7c3a40e1a8d42c4821bb6103871e
jeanlucqs <ojeanlucas.br@gmail.com>
2025-10-14 10:40:58 -0300

    NeyMar

--- mapa/urls.py
+++ mapa/urls.py
@@ -1,24 +1,11 @@
-
 from django.urls import path
-<<<<<<< HEAD
 from . import views
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('registro/', views.pagina_registro, name='registro'),
-    path('login/', views.pagina_login, name='login'),
+    path('login/', views.login, name='login'),
     path('logout/', LogoutView.as_view(next_page='index'), name='logout'),
     path('', views.index, name='index'), 
-    path('front/', views.pagina_front, name='pagina_front'),
+    path('front/', views.front, name='pagina_front'),
 ]
-=======
-from .views import index, pagina_principal, pagina_registro, pagina_login, pagina_front
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('login/', pagina_login, name='login'),
-    path('registro/', pagina_registro, name='registro'),
-    path('front/', pagina_front, name='pagina_front'),
-    path('login/principal/', pagina_principal, name='pagina_principal'),
-]
->>>>>>> 8d7a46320db8db6fe3c693d59c7a040b1dbc7f68
